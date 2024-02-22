7be5c122394cf49e44da74257df0c35af3753f26
kayoferreira123 <kayoferreira261200@gmail.com>
2024-02-22 08:00:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Exercicios para finalizar:
+
+//1 Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+//2 Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+//3 No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+//4 Substitua todas as classes js- por data atributes.
